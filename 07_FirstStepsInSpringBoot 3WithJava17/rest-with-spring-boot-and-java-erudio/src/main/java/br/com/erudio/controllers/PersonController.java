1c4852931db9d2b47acd622a1ec350708c2f235b
raf98 <rafaelrutzsantos@gmail.com>
2023-03-23 04:48:43 -0300

    Now actually added whole Persons project.

---
+++ 07_FirstStepsInSpringBoot 3WithJava17/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/PersonController.java
@@ -0,0 +1,29 @@
+package br.com.erudio.controllers;
+
+import br.com.erudio.model.Person;
+import br.com.erudio.services.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/person")
+public class PersonController {
+    @Autowired
+    private PersonService personService;
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Person findById(@PathVariable(value = "id") String id) {
+        return personService.findById(id);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Person> findAll() {
+        return personService.findAll();
+    }
+}
