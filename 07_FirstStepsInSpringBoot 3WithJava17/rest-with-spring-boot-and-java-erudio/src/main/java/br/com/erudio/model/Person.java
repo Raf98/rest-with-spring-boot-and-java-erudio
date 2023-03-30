d655325c5e591af5ea85f8a504b23e235c91de3c
raf98 <rafaelrutzsantos@gmail.com>
2023-03-30 05:52:01 -0300

    Started integration with MySQL database

--- 07_FirstStepsInSpringBoot 3WithJava17/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/model/Person.java
+++ 07_FirstStepsInSpringBoot 3WithJava17/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/model/Person.java
@@ -3,11 +3,21 @@ package br.com.erudio.model;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "person")
 public class Person implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name = "first_name", nullable = false, length = 80)
     private String firstName;
+    @Column(name = "last_name", nullable = false, length = 80)
     private String lastName;
+    @Column(nullable = false, length = 100)
     private String address;
+    @Column(nullable = false, length = 6)
     private String gender;
 
     public Person(){}
