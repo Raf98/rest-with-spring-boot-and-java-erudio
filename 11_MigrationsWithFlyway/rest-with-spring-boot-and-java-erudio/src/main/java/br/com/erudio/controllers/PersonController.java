6a8f3aae45c673e7b7c256268bd38c6d9057e5dd
raf98 <rafaelrutzsantos@gmail.com>
2023-04-15 23:01:19 -0300

    Finished Flyway and Custom JSON Serialization

---
+++ 11_MigrationsWithFlyway/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/PersonController.java
@@ -0,0 +1,48 @@
+package br.com.erudio.controllers;
+
+import br.com.erudio.data.vo.v1.PersonVO;
+import br.com.erudio.model.Person;
+import br.com.erudio.services.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/person")
+public class PersonController {
+    @Autowired
+    private PersonService personService;
+
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public PersonVO findById(@PathVariable(value = "id") Long id) {
+        return personService.findById(id);
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<PersonVO> findAll() {
+        return personService.findAll();
+    }
+
+    @PostMapping(
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public PersonVO create(@RequestBody PersonVO personVO) {
+        return personService.create(personVO);
+    }
+
+    @PutMapping(
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public PersonVO update(@RequestBody PersonVO personVO) {
+        return personService.update(personVO);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<?> delete(@PathVariable(value = "id") Long id) {
+        personService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
